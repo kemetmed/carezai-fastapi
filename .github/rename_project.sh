de27aed3a54d621b1e4a3df246acb41e85fcef2e
kemetmed <kemetmed@users.noreply.github.com>
2023-06-05 10:55:46 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="kemetmed"
+original_name="carezai_fastapi"
+original_urlname="carezai-fastapi"
+original_description="Awesome carezai_fastapi created by kemetmed"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv carezai_fastapi $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
